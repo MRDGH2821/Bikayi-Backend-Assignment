5cc53e07a56ca5caa152377a518035a61b30bfdf
MRDGH2821 <mihirrabade@gmail.com>
2022-02-01 15:14:18 +0530

    chore: fix source file

--- rollup.config.js
+++ rollup.config.js
@@ -4,7 +4,7 @@ import { terser } from 'rollup-plugin-terser';
 import pkg from './package.json';
 
 export default {
-	input: 'src/index.ts',
+	input: 'src/typescript/index.ts',
 	output: [{
 		format: 'esm',
 		file: pkg.module,
