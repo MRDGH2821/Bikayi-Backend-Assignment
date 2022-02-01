00ae3cdfbe0e6012c6c502a181deeaf95ac085af
MRDGH2821 <mihirrabade@gmail.com>
2022-02-01 11:06:24 +0530

    chore: update types

--- src/typescript/api/controller/customerController.ts
+++ src/typescript/api/controller/customerController.ts
@@ -1,25 +1,26 @@
+import { Request, Response } from "express";
 import * as mongoose from "mongoose";
 const Customer = mongoose.model("Customer");
-export function list_all_customers(req, res) {
+export function list_all_customers(req: Request, res: Response) {
 	Customer.find({}, (err, customer) => {
 		if (err) res.send(err);
 		res.json(customer);
 	});
 }
-export function create_customer(req, res) {
+export function create_customer(req: Request, res: Response) {
 	let new_customer = new Customer(req.body);
 	new_customer.save((err, customer) => {
 		if (err) res.send(err);
 		res.json(customer);
 	});
 }
-export function read_customer(req, res) {
-	Customer.findById(req.params.customerId, (err, customer) => {
+export function read_customer(req: Request, res: Response) {
+	Customer.findById(req.params.customerId, (req, res) => {
 		if (err) res.send(err);
 		res.json(customer);
 	});
 }
-export function update_customer(req, res) {
+export function update_customer(req: Request, res: Response) {
 	Customer.findOneAndUpdate(
 		{ _id: req.params.bookId },
 		req.body,
@@ -31,7 +32,7 @@ export function update_customer(req, res) {
 	);
 }
 
-export function delete_customer(req, res) {
+export function delete_customer(req: Request, res: Response) {
 	Customer.remove(
 		{
 			_id: req.params.bookId,
